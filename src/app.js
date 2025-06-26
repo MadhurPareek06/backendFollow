a21e1aecbbd75fa2b479a7aa588602b9ce18d2fb
Madhur Pareek <pareekmadhur06324@gmail.com>
2025-06-26 13:40:10 +0530

    add utils for api error and response

---
+++ src/app.js
@@ -0,0 +1,24 @@
+// import express from "express"
+ 
+// const app = express()
+
+// export {app}
+
+const express = require("express");
+const cors = require("cors");
+const cookieparser = require("cookie-parser");
+const { urlencoded } = require("body-parser");
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({limit: "16kb"}))
+app.use(urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieparser())
+
+module.exports = { app };
--- src/index.js
+++ src/index.js
@@ -9,9 +9,18 @@
 
 const dotenv = require("dotenv");
 const connectDB = require("./db/index");
+const { app } = require("./app");
 
 dotenv.config({
   path: "./.env",
 });
 
-connectDB();
+connectDB()
+.then(() => {
+  app.listen(process.env.PORT || 4000, () => {
+    console.log(`Listening on the port: ${process.env.PORT}`);
+  })
+})
+.catch((error) => {
+  console.log("MONGODB connection failed", error)
+})
